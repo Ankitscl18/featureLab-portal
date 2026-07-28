d9dc7d8aa4ef286954ba51887a2694f814579ca0
Ankit Kumar <ankitbelive@gmail.com>
2026-07-28 13:57:34 +0530

    use dynamic port for render depolyment

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ const nodemailer = require('nodemailer');
 const pool = require('./db');
 
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
